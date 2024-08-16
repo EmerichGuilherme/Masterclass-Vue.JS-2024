d1b4c95ccbfbd66fbb4ba06b92c590367d10c7af
emerichguilherme <guilhermeschuab2@gmail.com>
2024-08-15 23:17:09 -0300

    Criando primeiros componentes

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import primeiroComponente from './components/primeiroComponente.vue';
+import comDados from './components/comDados.vue'
+import comDadosReativosPrimitivos from './components/comDadosReativosPrimitivos.vue'
+import comDadosReativosNaoPrimitivos from './components/comDadosReativosNaoPrimitivos.vue';
+import dataBinding from './components/dataBinding.vue';
+import comProps from './components/comProps.vue';
+</script>
+
+<template>
+    <header>Vue</header>
+    <comProps item="Borracha" :preco="2.50"/>
+    <dataBinding/>
+    <comDadosReativosNaoPrimitivos/>
+    <comDadosReativosPrimitivos/>
+    <comDados/>
+    <primeiroComponente/>
+</template>
+
---
+++ src/components/comDadosReativosNaoPrimitivos.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="componente">
+        <h1>
+            Componente com dados Reativos Não Primitivos
+        </h1>
+        <p>Nome: {{ pessoa.nome }} | Idade: {{ pessoa.idade }}</p>
+        <button v-on:click="fazerAniversario()">Fazer Aniversário</button>
+        <button @click="mudarNome()">Mudar Nome</button>
+    </div>
+</template>
+
+<!-- <script setup>
+//composition API
+import {reactive} from "vue"
+
+const pessoa = reactive({nome:'Gui', idade:24})
+
+
+function fazerAniversario() {
+    pessoa.idade +=1
+}
+
+function mudarNome() {
+    pessoa.nome = "Ana Maria"
+}
+</script> -->
+
+<script>
+//option API
+export default {
+    data:function() {
+        return {
+            pessoa: {
+                nome: "Gui",
+                idade: 24
+            }
+        }
+    },
+    methods:{
+        fazerAniversario: function(){
+            this.pessoa.idade +=1
+        },
+        mudarNome: function() {
+            this.pessoa.nome = "Ana maria"
+        }
+    }
+}
+</script>
